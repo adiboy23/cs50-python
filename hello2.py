12165e06b1cf7bd6ce5cb7daf90775eea9c41aef
adiboy23 <121304804+adiboy23@users.noreply.github.com>
2023-02-06 12:21:38 +0530

    Create hello2.py

---
+++ hello2.py
@@ -0,0 +1,10 @@
+# Ask user for their name
+name = input("What's your name? " )
+
+# Say hello to user
+print("Hello, ", end="")
+
+# Default value of end is \n but in this we are overriding with end="".
+# READ - https://docs.python.org/3/library/functions.html#print
+
+print(name)
