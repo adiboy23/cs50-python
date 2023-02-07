7ca9d6335c9f87cf0b0e21137c1b4cbe71173013
adiboy23 <121304804+adiboy23@users.noreply.github.com>
2023-02-07 11:10:52 +0530

    Create indoor.py

---
+++ indoor.py
@@ -0,0 +1,8 @@
+# Take users input assuming all letters are in Capitals
+txt = input("Please enter your expression: ")
+
+# Transform all letters from caps to lowercase
+x = txt.lower()
+
+# print the lowercase output from above
+print(x)
