ccac84344dfb3dfd78a4812a44c2c6be1096a033
adiboy23 <121304804+adiboy23@users.noreply.github.com>
2023-02-07 23:09:14 +0530

    Create hello7.py

---
+++ hello7.py
@@ -0,0 +1,9 @@
+# Creating a function called hello 
+def hello(to):
+    print("hello,", to)
+
+#Take the users input into the name variable
+name = input("What is your name? ")
+
+#print hello and user's name in the name variable
+hello(name)
