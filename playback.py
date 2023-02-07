dc04bcec488a8f4e66459c06d89541bb6064704b
adiboy23 <121304804+adiboy23@users.noreply.github.com>
2023-02-07 11:05:48 +0530

    Create playback.py

---
+++ playback.py
@@ -0,0 +1,8 @@
+# Take users input in variable txt
+txt = input("Please enter your expression: ")
+
+# Replace the blank spaces in users input with ... and hold in new variable x
+x = txt.replace(" ", "...")
+
+# Print new variable x
+print(x)
